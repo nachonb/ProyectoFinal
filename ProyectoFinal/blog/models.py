4dce92679bf6654e8adc56080e5e0f4335ef65f4
Ignacio Nuñez Barreto <ignacio.n.b@hotmail.com>
2022-10-08 16:15:49 -0300

    Modelos

---
+++ ProyectoFinal/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+
+    class Meta: 
+        verbose_name_plural = "Autores"
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    profesion = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=30)
+    texto = models.CharField(max_length=1000)
+    fecha = models.DateField(null = True)
+
+class Seccion(models.Model):
+
+    class Meta: 
+        verbose_name_plural = "Secciones"
+
+    nombre = models.CharField(max_length=30)
+    
